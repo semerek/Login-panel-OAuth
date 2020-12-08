040952f9edcd38815ca7a6025ad08e653db7391a
Marta Semrau <mm.semrau@gmail.com>
2020-12-08 18:49:38 +0100

    Implement OAuth

--- server.js
+++ server.js
@@ -3,7 +3,35 @@ const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
 
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+
 const app = express();
+
+passport.use(new GoogleStrategy({
+  clientID: '938671182185-km7ihahkfs8lss4te7dnl13o33uqru7o.apps.googleusercontent.com',
+  clientSecret: 'fTYdna43ZxszC1P6tqSNn-Ez',
+  callbackURL: 'http://localhost:8000/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  done(null, profile);
+  console.log(profile)
+
+}));
+
+// serialize user when saving to session
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
+
+app.use(session({ secret: 'anything' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
@@ -12,6 +40,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
+
+
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -23,6 +53,19 @@ app.get('/user/logged', (req, res) => {
 
 app.get('/user/no-permission', (req, res) => {
   res.render('noPermission');
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
+  (req, res) => {
+    res.redirect('/user/logged');
+  }
+);
+
+app.get('/auth/google/callback', (req, res) => {
+  res.send(`I'm back from Google!`);
 });
 
 app.use('/', (req, res) => {
